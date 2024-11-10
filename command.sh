433db60c12be852bbc666fd3bad0725aacf02fc6
antoniudin1 <109304996+antoniudin1@users.noreply.github.com>
2024-11-09 19:16:35 -0500

    Update command.sh

--- command.sh
+++ command.sh
@@ -19,18 +19,12 @@ echo "#"
 mkdir TEST1
 cd TEST1
 
-#!/bin/bash
-
-# Prompt the user for input
 read -p "Please enter your name: " user_name
 
-# Print what the user entered
 echo "Hello, $user_name! Nice to meet you."
 
-# Another example: asking for a custom message
 read -p "Please enter a message: " user_message
 
-# Print the user's message
 echo "You just typed: $user_message"
 
 
